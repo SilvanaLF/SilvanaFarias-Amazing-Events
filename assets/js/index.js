2c6e55f0e48c3215c20ef66b2aa4f228bf263e50
SilvanaLF <sfcreativa@gmail.com>
2023-03-06 16:46:13 -0300

    Traer dinámicamente los nombres de las categorías en cada chexbox

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-console.log(dataEvents.events)
+//Cards
 
 let tarjetas = [];
 let fragment = document.createDocumentFragment();
@@ -20,8 +20,64 @@ cardEvent.innerHTML += `<div class="col ml-5">
                     </div>
                 </div>
             </div>`;
-    console.log(element)
 } 
+
+//Checkbox & Search
+
+//Agrego contenedor
+
+
+const busqueda = document.getElementById('categories');
+
+const newCategory =[];
+
+for (categoria of dataEvents.events){
+    newCategory.push(categoria.category);
+}
+
+const duplicateCategory = newCategory.filter((cat, indice) => {
+    return newCategory.indexOf(cat) !== indice;
+});
+
+const fragment01 = new DocumentFragment();
+
+for (let elemento of duplicateCategory){
+    let div = document.createElement('div');
+    div.innerHTML += `<div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" value="option">
+                        <label class="form-check-label" for="checkbox">${elemento} </label>
+                    </div>`
+                    
+    fragment01.appendChild(div);
+};
+
+busqueda.appendChild(fragment01);
+
+const hijos = busqueda.children;
+const arrayHijos = []
+
+const checks = document.getElementsByClassName('form-check-input')
+
+arrayHijos.push(checks);
+
+const arrayHijos2 = Array.prototype.slice.call(arrayHijos[0]);
+const etiquetas = Array.prototype.slice.call(document.getElementsByClassName('form-check-label'));
+
+const checkboxes = document.querySelectorAll('input[type=checkbox]')
+
+checkboxes.forEach( checkbox  => { 
+     checkbox.addEventListener('change', verificarSeleccion) 
+     })
+function verificarSeleccion(){
+     let inputheck = Array.from(checkboxes).filter(checkbox => checkbox.checked)
+     console.log(inputheck);
+}
+
+console.log(etiquetas);
+console.log(arrayHijos2);
+console.log(tarjetas);
+ 
+
 
 
 
--- assets/js/upcoming.js
+++ assets/js/upcoming.js
@@ -24,3 +24,55 @@ cardEvent.innerHTML += `<div class="col ml-5">
                 </div>
             </div>`;
 } 
+
+//Checkboxes & Search
+
+const busqueda = document.getElementById('categories');
+
+const newCategory =[];
+
+for (categoria of dataEvents.events){
+    newCategory.push(categoria.category);
+}
+
+const duplicateCategory = newCategory.filter((cat, indice) => {
+    return newCategory.indexOf(cat) !== indice;
+});
+
+const fragment01 = new DocumentFragment();
+
+for (let elemento of duplicateCategory){
+    let div = document.createElement('div');
+    div.innerHTML += `<div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" value="option">
+                        <label class="form-check-label" for="checkbox">${elemento} </label>
+                    </div>`
+                    
+    fragment01.appendChild(div);
+};
+
+busqueda.appendChild(fragment01);
+
+const hijos = busqueda.children;
+const arrayHijos = []
+
+const checks = document.getElementsByClassName('form-check-input')
+
+arrayHijos.push(checks);
+
+const arrayHijos2 = Array.prototype.slice.call(arrayHijos[0]);
+const etiquetas = Array.prototype.slice.call(document.getElementsByClassName('form-check-label'));
+
+const checkboxes = document.querySelectorAll('input[type=checkbox]')
+
+checkboxes.forEach( checkbox  => { 
+     checkbox.addEventListener('change', verificarSeleccion) 
+     })
+function verificarSeleccion(){
+     let inputheck = Array.from(checkboxes).filter(checkbox => checkbox.checked)
+     console.log(inputheck);
+}
+
+console.log(etiquetas);
+console.log(arrayHijos2);
+console.log(tarjetas);
