cff1c38e202a943cb040f4cb35a03a3141774d4e
SilvanaLF <sfcreativa@gmail.com>
2023-03-09 04:01:12 -0300

    Corregí conflictos con las id / Organicé con funciones / Filtros funcionan / No muestra las CARD DETAILS desde Upcoming y Past

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,36 +1,35 @@
-// Card detail URL
+//console.log([document])
 
 
-const queryString = location.search;
-console.log(queryString);
-const params = new URLSearchParams(queryString);
-console.log(params);
-const id = params.get("id");
-console.log(id);
+const queryString = location.search
+console.log(queryString)
+const params = new URLSearchParams(queryString)
+console.log(params)
+const id = params.get("id")
+console.log(id)
 
-const evento = dataEvents.events.find(element => element._id == id);
-console.log(evento);
+const evento = data.events.find(element => element.id == id)
+console.log(evento)
 
 
-function mostrarCard() {
-    const cardDetail = document.getElementById('detalleCard');
-    let boxCard = document.createElement('div')
-    boxCard.classList.add("card")
-    boxCard.classList.add("mt-3", "mb-3")
-    boxCard.style.width = "18rem"
-    boxCard.innerHTML = `<img src="${evento.image}" class="card-img-top" style="height: 150px" alt="Cinema">
-    <div class="card-body d-flex flex-column justify-content-center">
+function mostrarCard(){
+    const div = document.getElementById('detailsCard')
+    let detailsCard = document.createElement('div')
+    detailsCard.classList.add("card")
+    detailsCard.classList.add("mt-3", "mb-3")
+    detailsCard.style.width = "18rem"
+    detailsCard.innerHTML = `<img src="${evento.image}" class="card-img-top" style="height: 150px" alt="Race">
+    <div class="card-body d-flex flex-column justify-content-between">
     <h3 class="card-title">${evento.name}</h3>
     <p>Date: ${evento.date}</p>
     <p class="card-text">${evento.description}</p>
     <p>Place: ${evento.place}</p>
-    <p>Price: ${evento.price} u$d</p>
+    <p>Price: ${evento.price} S</p>
     <a href="./index.html" class="btn btn-dark nav-item p-2 me-1 ms-1 mb-1"
-        style="color: #d63384; background-color: #ededf2">Back</a>
+        style="color: #e0046c; background-color: #e9ecef">Back</a>
     </div>`
-
-    document.body.appendChild(boxCard);
+    div.appendChild(detailsCard)
 }
 
-mostrarCard();
+mostrarCard()
    
--- assets/js/upcoming.js
+++ assets/js/upcoming.js
@@ -1,138 +1,135 @@
-const actualDate = Date.parse(dataEvents.currentDate);
-console.log(dataEvents.events)
+// CARDS
 
-let tarjetas = [];
-let fragment = document.createDocumentFragment();
-const cardEvent = document.getElementById('card-template');
+function cardCreate(eventos){
 
-for(let cardE of dataEvents.events) {
-    if (Date.parse(cardE.date) > actualDate){
-        tarjetas.push(cardE);
-    }
-};
-
-for (let element of tarjetas) {
-cardEvent.innerHTML += `<div class="col ml-5">
-                <div class="card" style="width: 18rem;">
-                    <img src=${element.image} alt="cine">
-                    <div class="card-body">
-                        <h4>${element.name}</h4>
-                        <p class="card-text">${element.description}</p>
-                        <p class="card-text"><small class="text-muted">${element.price}</small></p>
-                        <a href="./details.html?id=${element._id}" class="btn btn details align-self-end">Details</a>
-                    </div>
-                </div>
-            </div>`;
-} 
-
-//Checkbox & Search
-
-//Checkbox
-//Traer dinámicamente los nombres de las categorías en cada chexbox
-
-
-const busqueda = document.getElementById('categories');
-
-const newCategory =[];
-
-for (categoria of dataEvents.events){
-    newCategory.push(categoria.category);
-}
-
-const duplicateCategory = newCategory.filter((cat, indice) => {
-    return newCategory.indexOf(cat) !== indice;
-});
-
-const fragment01 = new DocumentFragment();
-
-for (let element of duplicateCategory){
-    let div = document.createElement('div');
-    div.innerHTML += `<div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" id="${element}" value=${element}>
-                        <label class="form-check-label" for=${element}>${element} </label>
-                    </div>`
-                    
-    fragment01.appendChild(div);
-};
-
-busqueda.appendChild(fragment01);
-
-const hijos = busqueda.children;
-const arrayHijos = []
-
-const checks = document.getElementsByClassName('form-check-input')
-
-arrayHijos.push(checks);
-
-const arrayHijos2 = Array.prototype.slice.call(arrayHijos[0]);
-const etiquetas = Array.prototype.slice.call(document.getElementsByClassName('form-check-label'));
-
-//Escuchar eventos por categorías(filtros)
-
-busqueda.addEventListener('click', imprimirPorConsola)
-    
-function imprimirPorConsola(e){
-     console.log(e.target)
- }
-
-const checkboxes = document.querySelectorAll('input[type=checkbox]');
-checkboxes.forEach( checkbox  => {checkbox.addEventListener('change', verificarSeleccion)});
-
-function verificarSeleccion(){
-     let inputcheck = Array.from(checkboxes).filter(checkbox => checkbox.checked)
-     console.log(inputcheck);
-     let valorCategories = inputcheck.map(input => input.value)
-     console.log(valorCategories);
-     let eventosFiltros = dataEvents.events.filter(evento => valorCategories.includes(evento.category.split(' ').join('_')));
-     console.log(eventosFiltros)
-     filteredCards(eventosFiltros, cardEvent);
-}
-
-//CARDS DE CATEGORIAS FILTRADAS
-
-
-function filteredCards(array, cardEvent){
-    const container = cardEvent;
+    const container = document.getElementById('card_up')
     container.innerHTML=''
+    if(eventos.length > 0){
     let fragment = document.createDocumentFragment()
 
-    for(let element of array){
-        let tarjetas = document.createElement('div');
-        tarjetas.innerHTML =  `<div class="col ml-5">
-        <div class="card" style="width: 18rem;">
-            <img src=${element.image} alt="cine">
-            <div class="card-body">
-                <h4>${element.name}</h4>
-                <p class="card-text">${element.description}</p>
-                <p class="card-text"><small class="text-muted">${element.price}</small></p>
-                <a href="./details.html?id=${element._id}" class="btn btn details align-self-end">Details</a>
-            </div>
-        </div>
-    </div>`
-    fragment.appendChild(tarjetas)
+    for(let element of eventos){
+        let div = document.createElement('div')
+        div.classList.add("card")
+        div.style.width = "18rem"
+        div.innerHTML = `<img src="${element.image}" class="card-img-top" style="height: 150px" alt="Race">
+        <div class="card-body d-flex flex-column justify-content-between">
+        <h3 class="card-title">${element.name}</h3>
+        <p class="card-text">${element.description}</p>
+        <p>Price: ${element.price} $</p>
+        <a href="./details.html?id=${element.id}" class="btn btn-dark nav-item p-2 me-1 ms-1 mb-1"
+            style="color: #e0046c; background-color: #e9ecef">Detail</a>
+        </div>`
+        fragment.appendChild(div)
+        }
+        container.appendChild(fragment)
+    }else{
+        let div = document.createElement('div')
+        div.innerHTML = `<p class="card-text">¡Que comience el matriarcado!</p>`
+        container.appendChild(div)
     }
-    container.appendChild(fragment)
 }
+const card_up = document.getElementById('card_up')
 
-  
+let fragment = document.createDocumentFragment()
+
+const fechaActual = Date.parse(data.currentDate);
+
+for(let element of data.events){
+    
+    let proximasFechas = Date.parse(element.date)
+
+    if(proximasFechas > fechaActual){
+    let div = document.createElement('div')
+        div.classList.add("card")
+        div.style.width = "18rem"
+        div.innerHTML = `<img src="${element.image}" class="card-img-top" style="height: 150px" alt="Race">
+    <div class="card-body d-flex flex-column justify-content-between">
+      <h3 class="card-title">${element.name}</h3>
+      <p class="card-text">${element.description}</p>
+      <p>Price: ${element.price} u$d</p>
+      <a href="./details.html" class="btn btn-dark nav-item p-2 me-1 ms-1 mb-1"
+        style="color: #e0046c; background-color: #e9ecef">Detail</a>
+    </div>`
+    fragment.appendChild(div)
+    }
+}
+card_up.appendChild(fragment)
+
+
+//CHECKBOX
+
+const check_category = document.getElementById('cat-check')
+
+function checkbox(events) {
+
+    const arrayCategoria = events.map(event => event.category)
+    const theCategory = [...new Set(arrayCategoria)]
+
+
+let fragmentCheck = document.createDocumentFragment()
+
+ for(let cat of theCategory){
+     let div = document.createElement('div')
+     div.className="form-check form-check-inline"
+     div.style.color="#e0046c"
+     div.innerHTML = `<input class="form-check-input" type="checkbox" id=${cat.split(' ').join('_')} value=${cat.split(' ').join('_')}>
+     <label class="form-check-label" style="color: #e0046c" for=${cat.split(' ').join('_')}>${cat}</label>`
+         fragmentCheck.appendChild(div)
+ }
+ return fragmentCheck
+ }
+
+check_category.appendChild(checkbox(data.events))
+
+//EVENTOS CHECKBOX
+
+
+let inputValues = []
+let textSearch = ""
+
+const checkboxes = document.querySelectorAll('input[type=checkbox]')
+
+ checkboxes.forEach(checkbox => {
+     checkbox.addEventListener('change', mostrarSeleccion)
+ })
+
+ function mostrarSeleccion(eventos){
+    inputValues = Array.from(checkboxes).filter(check => check.checked).map(input => input.value)
+    allFilter(data.events)
+
+ }
+ 
+ function arrayFilter(checkArray, events){
+    if(checkArray == 0){
+        return events
+    }else{
+    const eventosFiltrados = events.filter(objeto => checkArray.includes(objeto.category.split(' ').join('_')))
+        return eventosFiltrados
+    }
+ }
 
 //SEARCH
 
+function filterSearch(string, events){
+    if(string == ""){
+       return events
+    }else{         
+    let nuevoArray = events.filter(element => element.name.toLowerCase().includes(textSearch.toLowerCase().trim()))
+    return nuevoArray
+   }
+}
 
-const searchInput = document.getElementById("searchInput")
-//console.log(searchInput)
-
-searchInput.addEventListener('keyup', () =>{
-    const textoDeBusqueda = searchInput.value
-    console.log(textoDeBusqueda)
-    console.log(filterTexto(textoDeBusqueda))
+const inputForm = document.getElementById('inputForm');
+inputForm.addEventListener('keyup', (e) => {
+textSearch = inputForm.value
+allFilter(data.events)
 })
 
-function filterTexto (textoDeBusqueda){
-    if (textoDeBusqueda == '') return dataEvents.events
-    let nuevoArray = dataEvents.events.filter(elemento => elemento.name.toLowerCase().includes(textoDeBusqueda.toLowerCase().trim()))
-    return nuevoArray
-}
-  
-
 //FILTROS CRUZADOS
+
+
+function allFilter(){
+ const eventosChequeados = arrayFilter(inputValues, data.events)
+ const eventosBuscados = filterSearch(textSearch, eventosChequeados)
+ cardCreate(eventosBuscados)
+}
